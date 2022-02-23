720378f657dae7c7da7cafd3a3120be346c657ca
Amatul Sharin <sharinamatul@gmail.com>
2022-02-23 20:06:03 +0700

    Changes: - Added an clickable icon for profile    > The problem that made the icon didn't show was because: I only made the menu, but I haven't called that menu so that it'd appear    > It's finally clickable by overriding onOptionsItemSelected method (my confusion is in the code)
    - Changed the layout of each list elements, as you can see :)
    
    My big accomplishments:
    - I figured out how to make the card clickable, but doesn't show that there is a margin ... And I made it! When I click the card, the "Feedback" only appears inside the card, so it's not obvious that I have margin ... Hehehe~
    
    Now, time to do some more!

--- app/src/main/java/com/sharinamatul/android/dicodingsubmission/BinatangAdapter.java
+++ app/src/main/java/com/sharinamatul/android/dicodingsubmission/BinatangAdapter.java
@@ -1,25 +1,47 @@
 package com.sharinamatul.android.dicodingsubmission;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinatangAdapter extends ArrayAdapter<InfoBinatang> {
 
 
-    public BinatangAdapter(@NonNull Context context, @NonNull List<InfoBinatang> objects) {
+    public BinatangAdapter(@NonNull Context context, @NonNull ArrayList<InfoBinatang> objects) {
         super(context, 0, objects);
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.card_view, parent, false);
+        }
+
+        InfoBinatang currentInfoBinatang = getItem(position);
+
+        ImageView fotoBinatang = listItemView.findViewById(R.id.foto);
+        fotoBinatang.setImageResource(R.mipmap.ic_launcher);
+
+        TextView judul = listItemView.findViewById(R.id.judul);
+        judul.setText("Ini Judul");
+
+        TextView deskripsi = listItemView.findViewById(R.id.deskripsi);
+        deskripsi.setText("tools:text=\"Pretend that this is something I really want to pursue but what if I cannot achieve that? that'd be embarassing.\"\n");
+
+        return listItemView;
     }
 }
--- app/src/main/java/com/sharinamatul/android/dicodingsubmission/MainActivity.java
+++ app/src/main/java/com/sharinamatul/android/dicodingsubmission/MainActivity.java
@@ -1,14 +1,36 @@
 package com.sharinamatul.android.dicodingsubmission;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Kenapa pakai item coba? Bukan view, layout, or sth?
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.profile_button:
+                Toast.makeText(this, "Profile is clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_file, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +38,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Membuat ArrayList untuk menampung semua informasi tentang hewan
-        ArrayList<String> binatang = new ArrayList<>();
+        ArrayList<InfoBinatang> binatang = new ArrayList<>();
 
-        binatang.add("Harimau");
-        binatang.add("Macan");
+        binatang.add(new InfoBinatang("Sasatoan", "Apa gitu terserah", R.mipmap.ic_launcher_round));
+        binatang.add(new InfoBinatang("Sasatoan", "Apa gitu terserah", R.mipmap.ic_launcher_round));
+        binatang.add(new InfoBinatang("Sasatoan", "Apa gitu terserah", R.mipmap.ic_launcher_round));
 
         //binatang.add(new InfoBinatang("Androchan", "Androchan punah saat perjalanannya menuju Bandung.", R.mipmap.ic_launcher_round));
 
         //Menggunakan ArrayAdapter untuk mengambil tiap elemen, dan menampilkannya di ListView
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, binatang);
+        BinatangAdapter adapter = new BinatangAdapter(this, binatang);
 
         //ListView untuk menggunakan layout dari "ArrayAdapter" dan menampilkannya dlm bentuk list
         ListView listView = findViewById(R.id.listview);
